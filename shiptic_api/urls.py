5b6d3752149a5adfd7fd294ac4fcbbeeb3444852
jakebuhite <jakebuhite@gmail.com>
2025-01-14 19:43:03 -0500

    Added health check

--- shiptic_api/urls.py
+++ shiptic_api/urls.py
@@ -6,12 +6,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from apps.accounts import views as account_views
 
+from health_check.views import HealthCheckView
+
 router = routers.DefaultRouter()
 
 router.register(r'users', account_views.UserViewSet)
 router.register(r'businesses', account_views.BusinessViewSet)
 
 urlpatterns = [
+    path('health/', HealthCheckView.as_view(), name='health_check'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
