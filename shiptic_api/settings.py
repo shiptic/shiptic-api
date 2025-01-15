a6c189c27be2136210b722830222e62bb701bf31
jakebuhite <jakebuhite@gmail.com>
2025-01-14 20:01:48 -0500

    Updated health check

--- shiptic_api/settings.py
+++ shiptic_api/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'health_check',
+    'health_check.db',
+    'health_check.cache',
+    'health_check.storage',
 ]
 
 MIDDLEWARE = [
