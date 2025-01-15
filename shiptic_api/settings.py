99932dfa3f3a62ca0e72e489779bf4f6fe53cf41
Christian Totaro <christian.totaro@stvincent.edu>
2025-01-14 19:00:58 -0500

    Updated database settings for beanstalk

--- shiptic_api/settings.py
+++ shiptic_api/settings.py
@@ -84,16 +84,28 @@ WSGI_APPLICATION = 'shiptic_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shiptic_db',                      
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': 'localhost',
-        'PORT': '5432',
+if os.environ.get('ELASTIC_BEANSTALK'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DB_NAME'],                      
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASS'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'shiptic_db',                      
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASS'],
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
